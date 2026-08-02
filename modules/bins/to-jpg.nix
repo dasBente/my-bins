3e4b0ba80dbe9033e82b77f9f2589b077f9d9f04
dasBente <dasbente@gmail.com>
2026-08-02 15:33:50 +0200

    feat: implement .desktop file

--- modules/bins/to-jpg.nix
+++ modules/bins/to-jpg.nix
@@ -3,14 +3,12 @@
     packages.to-jpg = pkgs.writeShellApplication {
       name = "to-jpg";
       runtimeInputs = [pkgs.coreutils pkgs.findutils pkgs.imagemagick];
-      text = builtins.readFile ./_src/to-jpg.sh;
+      text = builtins.readFile ./to-jpg.sh;
     };
     packages.to-jpg-servicemenu = pkgs.writeTextFile {
       name = "to-jpg-servicemenu";
       destination = "/share/kio/servicemenus/to-jpg.desktop";
-      text = ''
-        [Desktop Entry]
-      '';
+      text = builtins.readFile ./to-jpg.desktop;
     };
   };
 }
